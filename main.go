0347d060b65f0e945556c7d633d2ff12b0938b2c
ChanonNon <52731328+ChanonNon@users.noreply.github.com>
2022-10-29 04:36:19 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 // ไฟล์ทุกไฟล์ต้องเริ่มต้นที่ func main() ใน package main เสมอ
 package main
- 
+
 import "fmt"
 
-func main(){
+func main() {
 	fmt.Println("hello Gopher!!!")
 
-	fmt.Printf("hello %s!!!!!\n","Gopher")
-	fmt.Printfln("next line")
+	fmt.Printf("hello %s!!!!!\n", "Gopher")
+	fmt.Println("next line")
 
-	fmt.Printf("hello %s!!!!!\n","Gopher")
+	fmt.Printf("hello %s!!!!!\n", "Gopher")
 
-	fmt.Printf("hello %s!!!!!\n","Gopher")
-	fmt.Printf("hello %s!!!!!\n","Gopher")
+	fmt.Printf("hello %s!!!!!\n", "Gopher")
+	fmt.Printf("hello %s!!!!!\n", "Gopher")
 
-	fmt.Printf("hello %s!!!!!\n","Gopher")
+	fmt.Printf("hello %s!!!!!\n", "Gopher")
 
 }
